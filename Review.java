ccaea97b0820c629e19f215e5e0010f669ecbc5f
maxidler <43016462+Maxwell12345@users.noreply.github.com>
2020-01-15 22:46:57 -0500

    Update Review.java

--- Review.java
+++ Review.java
@@ -5,6 +5,30 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.io.*;
 
+
+//    Activity two reponses
+/**
+ * 4. 
+ * Firstly, the file containing the review in imputted as a parameter and is converted to a string using the textToString method
+ * A netReview value variable was defined as well as a placeholder for each word in the review
+ * The placeholder variable is defined by passing the review through a for loop, where if at a particular index the char is a space,
+ * the placeholder word is passed through the sentimentVal method and the value is added onto the netRev variable
+ * This process is done over and over again until the string is completely gone through
+ * The netRev is returned at the end
+ * 
+ * 5.
+ * a. Yes, the ratings make sense because I tested my function my printing the sentament value and ensuring that the value correlates
+ * to the proper rating defined in the assignement
+ * b. One way that I can edit the totalSentiment method is by perhaps using a logistic function to "average" out all the sentiments in the cvc file
+ * or I could use a logistic function or perhaps multiply the end sentiment by some constant to reduce the severity of the final result
+ * 
+ * 6. 
+ * a. The total sentament can be any double value because the value is determenined by the number of words in the review, and a review can be any length
+ * In the method, it defaults to 0 if the sentament is more than 15, and a review of 0 is bad, but a Tot Sent of > 15 is good
+ * b. He could fix this by adding an if statement with the expression (totalSentiment > 15) and returns 5, and then make the first initializing if statment 
+ * to an else if statement
+ */
+
 /**
  * Class that contains helper methods for the Review Lab
  **/
@@ -114,26 +138,6 @@ public class Review {
     return punc;
   }
   
-   /**
-   * Returns the word after removing any beginning or ending punctuation
-   */
-  public static String removePunctuation( String word )
-  {
-    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(0)))
-    {
-      word = word.substring(1);
-    }
-    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(word.length()-1)))
-    {
-      word = word.substring(0, word.length()-1);
-    }
-    
-    return word;
-  }
- 
-
-
-  
   /** 
    * Randomly picks a positive adjective from the positiveAdjectives.txt file and returns it.
    */
@@ -165,4 +169,40 @@ public class Review {
       return randomNegativeAdj();
     }
   }
+
+  public static double totalSentiment(String fileName)
+  {
+    double netRev = 0;
+    String temp = "";
+    String text = textToString(fileName);
+      
+    text = text.replaceAll("\\p{Punct}", "");
+
+      
+    for(int i = 0; i < text.length(); ++i)
+    {
+      if(text.substring(i, i+1).equals(" "))
+      {
+        netRev += Review.sentimentVal(temp);
+
+        temp = "";
+      }
+      else temp += text.substring(i, i+1);
+    }
+    return netRev;
+  }
+
+  public static int starRating(String fileName)
+  {
+    String text = textToString(fileName);
+    double round = 2.5 * Math.tanh(totalSentiment(text)) + 3;
+    return (int)round;
+  }
+
+  public static void main(String args[])
+  {
+    System.out.println(Review.totalSentiment("26WestReview.txt"));
+  }
+
 }
+
